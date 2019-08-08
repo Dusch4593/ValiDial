1b59075472fe6ce52fb2a77aea357ded92c9f637
Dusch4593 <brandondusch@gmail.com>
2019-08-07 22:13:06 -0400

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# ValiDial
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/Dusch4593/pen/OKVjbG](https://codepen.io/Dusch4593/pen/OKVjbG).
+
+ 
---
+++ src/script.js
@@ -0,0 +1,11 @@
+function validial() {
+  // Good luck!
+  let input = document.getElementById("dial_button").value;
+  let regex = /^(1( |-)?)?((\([0-9]{3}\))|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/;
+  if(regex.test(input)){
+    document.getElementById("output").innerHTML = "&#10003; It's Valid!!!";
+  }else{
+    document.getElementById("output").innerHTML = "&#9747; It's Not Valid!!!";
+  }
+}
+
